573d73debbc7976e7109922c2d035577a50518ef
Sorin <soringoredev@gmail.com>
2026-05-16 12:53:55 +0300

    Initial commit

---
+++ src/main/java/example/com/Spring/Boot/Starters/Project/SpringBootStartersProjectApplication.java
@@ -0,0 +1,13 @@
+package example.com.Spring.Boot.Starters.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartersProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartersProjectApplication.class, args);
+	}
+
+}
